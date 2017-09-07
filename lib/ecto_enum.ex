b98cf73eaed0d9f04936f7f032f35328236424c4
Cesario Uy <cesario@homepolish.com>
2017-09-07 01:22:01 -0400

    Support string enums

--- lib/ecto_enum.ex
+++ lib/ecto_enum.ex
@@ -75,13 +75,13 @@ defmodule EctoEnum do
         @string_atom_map for {atom, int} <- kw, into: %{}, do: {Atom.to_string(atom), atom}
         @valid_values Keyword.values(@atom_int_kw) ++ Keyword.keys(@atom_int_kw) ++ Map.keys(@string_int_map)
 
-        def type, do: :integer
+        def type, do: :string
 
         def cast(term) do
           EctoEnum.cast(term, @int_atom_map, @string_atom_map)
         end
 
-        def load(int) when is_integer(int) do
+        def load(int) do
           Map.fetch(@int_atom_map, int)
         end
 
@@ -112,28 +112,24 @@ defmodule EctoEnum do
       :error
     end
   end
-  def cast(string, _, string_atom_map) when is_binary(string) do
-    Map.fetch(string_atom_map, string)
+  def cast(val, int_atom_map, string_atom_map) do
+    if val in Map.keys(int_atom_map) do
+      Map.fetch(int_atom_map, val)
+    else
+      Map.fetch(string_atom_map, val)
+    end
   end
-  def cast(int, int_atom_map, _) when is_integer(int) do
-    Map.fetch(int_atom_map, int)
-  end
-  def cast(_, _, _), do: :error
 
 
   @spec dump(any, [{atom(), any()}], map, map) :: {:ok, integer} | :error
-  def dump(integer, _, _, int_atom_map) when is_integer(integer) do
-    if int_atom_map[integer] do
-      {:ok, integer}
-    else
-      :error
-    end
-  end
   def dump(atom, atom_int_kw, _, _) when is_atom(atom) do
     Keyword.fetch(atom_int_kw, atom)
   end
-  def dump(string, _, string_int_map, _) when is_binary(string) do
-    Map.fetch(string_int_map, string)
+  def dump(val, _, string_int_map, int_atom_map) do
+    if val in Map.keys(int_atom_map) do
+      {:ok, val}
+    else
+      Map.fetch(string_int_map, val)
+    end
   end
-  def dump(_), do: :error
 end
